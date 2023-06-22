5a0a6f714fbd1c28197d38aa72b5133e110d56a3
Atsushi0915 <w_atsushi_0915@outlook.jp>
2023-06-22 19:46:57 +0900

    入力機能と削除機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,39 @@ import React, { useState } from "react";
 import "./styles.css"
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incpmpleteTodos, setIncompleteTodos] = useState(['あああああああ', 'いいいいいいい']);
   const [completeTodos, setCompleteTodos] = useState(['ううううううう'])
+
+  const onChangeTodoText = (e) => setTodoText(e.target.value)
+  const onClickAdd = () => {
+    if (todoText === '') return;
+    const newTodos = [...incpmpleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText('');
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incpmpleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力"/>
-        <button>追加</button>
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incpmpleteTodos.map((todo) => {
+          {incpmpleteTodos.map((todo, index) => {
             return(
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             )
           })}
