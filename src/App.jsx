fef83fb8c828f5c1dc3cc09649a75b30c55ebdd7
Atsushi0915 <w_atsushi_0915@outlook.jp>
2023-06-24 22:28:11 +0900

    タスク個数によるバリデーション追加

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,10 @@ export const App = () => {
   const [incompleteTodos, setIncompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([])
 
+  // テキスト入力値の値 
   const onChangeTodoText = (e) => setTodoText(e.target.value)
+
+  // 追加ボタンクリック時に「incompleteTodos」の配列にtodoを追加。inputボックス内のテキストを初期化。
   const onClickAdd = () => {
     if (todoText === '') return;
     const newTodos = [...incompleteTodos, todoText];
@@ -18,12 +21,15 @@ export const App = () => {
     setTodoText('');
   };
 
+  // 削除ボタンクリック時に「incompleteTodos」の配列から対象のtodoを削除。
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
   };
 
+  // 完了ボタンクリック時に「incompleteTodos」から対象のタスクを削除し、
+  // 「CompleteTodos」の配列へ完了したtodoを追加。
   const onClickComplete = (index) => {
     const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1)
@@ -33,6 +39,8 @@ export const App = () => {
     setCompleteTodos(newCompleteTodos);
   }
 
+  // 戻すボタンクリック時に「CompleteTodos」から対象のtodoを削除し、
+  // 「incompleteTodos」の配列へ対象のtodoを追加する。
   const onClickBack = (index) => {
     const newCompleteTodos = [...completeTodos];
     newCompleteTodos.splice(index, 1);
@@ -46,7 +54,11 @@ export const App = () => {
     <>
       <InputTodo todoText={todoText} 
                  onChange={onChangeTodoText} 
-                 onClick={onClickAdd} />
+                 onClick={onClickAdd}
+                 disabled={incompleteTodos.length >= 5} />
+      {incompleteTodos.length >= 5 && 
+        <p style={{ color: 'red' }}>登録できるtodo5個までだよ〜。消化しろ〜。</p>
+      }
       <IncompleteTodos todos={incompleteTodos} 
                        onClickComplete={onClickComplete}
                        onClickDelete={onClickDelete} />
